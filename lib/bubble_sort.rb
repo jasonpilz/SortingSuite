c10eaf13b5c9adf2453a06666c410ec573dd7aea
Jason Pilz <jasonpilz@gmail.com>
2015-08-28 20:33:22 -0600

    Initial Working Version

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,7 +1,39 @@
 module SortingSuite
+  # This class is used to sort a given array of numbers
   class BubbleSort
-    def initialize
+    attr_reader :numbers
 
+    def initialize(numbers)
+      @numbers = numbers
+    end
+
+    def sort
+      # Check if sorting is needed
+      return numbers if numbers.size < 2
+
+      bubble = numbers.size
+
+      (bubble).times do
+        index = 0
+        until index == numbers.count - 1
+          previous = numbers[index]
+          current = numbers[index + 1]
+
+          # Swap
+          if current < previous
+            numbers[index] = current
+            numbers[index + 1] = previous
+          end
+
+          # Increase index to reference next pair in array
+          index += 1
+        end
+      end
+
+      return numbers
     end
   end
 end
+
+# bubble_sort = SortingSuite::BubbleSort.new([4, 2, 5, 3, 1])
+# bubble_sort.sort
--- test/bubble_sort_test.rb
+++ test/bubble_sort_test.rb
@@ -1,9 +1,54 @@
 require_relative '../lib/bubble_sort.rb'
 
+# This class is used to test the BubbleSort class
 class BubbleSortTest < Minitest::Test
-
-  def test_this
-    skip
+  def test_sort_array_of_one
+    bubble_sort = SortingSuite::BubbleSort.new([2])
+    assert_equal [2], bubble_sort.sort
   end
 
+  def test_sort_array_of_two_already_sorted
+    bubble_sort = SortingSuite::BubbleSort.new([3, 4])
+    assert_equal [3, 4], bubble_sort.sort
+  end
+
+  def test_sort_array_of_two_not_sorted
+    bubble_sort = SortingSuite::BubbleSort.new([4, 2])
+    assert_equal [2, 4], bubble_sort.sort
+  end
+
+  def test_sort_array_of_three_already_sorted
+    bubble_sort = SortingSuite::BubbleSort.new([1, 2, 3])
+    assert_equal [1, 2, 3], bubble_sort.sort
+  end
+
+  def test_sort_array_of_three_not_sorted_one_swap
+    bubble_sort = SortingSuite::BubbleSort.new([2, 1, 3])
+    assert_equal [1, 2, 3], bubble_sort.sort
+  end
+
+  def test_sort_array_of_three_not_sorted
+    bubble_sort = SortingSuite::BubbleSort.new([3, 2, 1])
+    assert_equal [1, 2, 3], bubble_sort.sort
+  end
+
+  def test_sort_array_of_four_already_sorted
+    bubble_sort = SortingSuite::BubbleSort.new([2, 3, 4, 5])
+    assert_equal [2, 3, 4, 5], bubble_sort.sort
+  end
+
+  def test_sort_array_of_four_not_sorted
+    bubble_sort = SortingSuite::BubbleSort.new([5, 2, 1, 0])
+    assert_equal [0, 1, 2, 5], bubble_sort.sort
+  end
+
+  def test_sort_array_of_five_already_sorted
+    bubble_sort = SortingSuite::BubbleSort.new([3, 4, 5, 6, 7])
+    assert_equal [3, 4, 5, 6, 7], bubble_sort.sort
+  end
+
+  def test_sort_array_of_five_not_sorted
+    bubble_sort = SortingSuite::BubbleSort.new([4, 2, 5, 3, 1])
+    assert_equal [1, 2, 3, 4, 5], bubble_sort.sort
+  end
 end
